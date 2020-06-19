7080e742acbd6641138bfadb2f55cbd6b16567d3
linuxdaemon <linuxdaemon.irc@gmail.com>
2020-06-19 10:15:13 -0400

    General code cleanup

--- updater.py
+++ updater.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 """
 Usage:
     updater.py check <repo> <path>
@@ -6,24 +7,24 @@ Usage:
 import os
 import sys
 import tarfile
-from pathlib import Path
-
-import requests
 from distutils.version import StrictVersion
-from docopt import docopt
+from pathlib import Path
 from shutil import rmtree
 
-ASSET_ID_FILE = '.asset_id'
-CURRENT_PATH = 'current'
-HISTORY_COUNT = 5
-RELEASES_PATH = 'releases'
-RELEASE_PREFIX = 'website-v'
+import requests
+from docopt import docopt
 
-RELEASE_URL = 'https://api.github.com/repos/{repo}/releases'
+ASSET_ID_FILE = ".asset_id"
+CURRENT_PATH = "current"
+HISTORY_COUNT = 5
+RELEASES_PATH = "releases"
+RELEASE_PREFIX = "website-v"
+
+RELEASE_URL = "https://api.github.com/repos/{repo}/releases"
 
 
 def _release_filter(release):
-    return not release['draft'] and not release['prerelease'] and release['assets']
+    return not release["draft"] and not release["prerelease"] and release["assets"]
 
 
 def get_latest_release_asset(repo):
@@ -36,12 +37,12 @@ def get_latest_release_asset(repo):
     if release is None:
         raise ValueError("Unable to find matching release")
 
-    return release['assets'][0]
+    return release["assets"][0]
 
 
 def check_for_update(args):
-    repo = args['<repo>']
-    deploy_path = Path(args['<path>']).resolve()
+    repo = args["<repo>"]
+    deploy_path = Path(args["<path>"]).resolve()
     if not deploy_path.is_dir():
         sys.exit("Website not deployed")
 
@@ -56,44 +57,41 @@ def check_for_update(args):
         sys.exit("No assets currently deployed")
 
     asset = get_latest_release_asset(repo)
-    if str(asset['id']) != current_id:
+    if str(asset["id"]) != current_id:
         sys.exit("Assets out of date")
 
     sys.exit(0)
 
 
 def do_update(args):
-    deploy_path = Path(args['<path>']).resolve()
+    deploy_path = Path(args["<path>"]).resolve()
     releases_dir = deploy_path / RELEASES_PATH
     current_link = deploy_path / CURRENT_PATH
 
-    asset = get_latest_release_asset(args['<repo>'])
+    asset = get_latest_release_asset(args["<repo>"])
 
     # Requires that the asset be '<name>.tar.gz'
-    release_name = asset['name'].rsplit('.', 2)[0]
+    release_name = asset["name"].rsplit(".", 2)[0]
 
     release_dir = releases_dir / release_name
     if not release_dir.exists():
         release_dir.mkdir(parents=True)
 
     with requests.get(
-            asset['url'], stream=True,
-            headers={
-                'Accept': 'application/octet-stream',
-            }
+        asset["url"], stream=True, headers={"Accept": "application/octet-stream",}
     ) as response:
-        with tarfile.open(fileobj=response.raw, mode='r|*') as tarball:
+        with tarfile.open(fileobj=response.raw, mode="r|*") as tarball:
             tarball.extractall(path=str(release_dir))
 
     # Record the ID of this asset for later update checks
-    with open(str(release_dir / ASSET_ID_FILE), 'w') as f:
-        print(asset['id'], file=f)
+    with open(str(release_dir / ASSET_ID_FILE), "w") as f:
+        print(asset["id"], file=f)
 
     if current_link.is_symlink():
         current_link.unlink()
 
     current_link.symlink_to(release_dir, target_is_directory=True)
-    print('Deployed', release_name)
+    print("Deployed", release_name)
 
     do_cleanup(releases_dir, release_name)
 
@@ -102,11 +100,11 @@ def do_cleanup(releases_dir, latest_release):
     directories = os.listdir(releases_dir)
 
     if len(directories) <= HISTORY_COUNT:
-        print('Did not run clean up (too few historic releases)')
+        print("Did not run clean up (too few historic releases)")
         return
 
     def key(d):
-        return StrictVersion(d.replace(RELEASE_PREFIX, ''))
+        return StrictVersion(d.replace(RELEASE_PREFIX, ""))
 
     directories.sort(key=key, reverse=True)
 
@@ -115,21 +113,21 @@ def do_cleanup(releases_dir, latest_release):
         rmtree(releases_dir / directory)
 
     if to_remove:
-        print(f'Cleaned up {len(to_remove)} historic releases')
+        print(f"Cleaned up {len(to_remove)} historic releases")
     else:
-        print('No clean up required')
+        print("No clean up required")
 
 
 def main():
     args = docopt(__doc__)
 
-    if args['check']:
+    if args["check"]:
         check_for_update(args)
-    elif args['update']:
+    elif args["update"]:
         do_update(args)
     else:
         raise ValueError("Invalid arguments from docopt")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
